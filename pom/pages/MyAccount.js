9ae46a3ddbb2a8a20fc3728fdf4dfecc6b702614
VirginiaZambudio770 <virginia.zambudio@gmail.com>
2024-10-31 17:43:40 -0300

    Fixes to reuse, main and encapsulation

--- pom/pages/MyAccount.js
+++ pom/pages/MyAccount.js
@@ -7,6 +7,5 @@ class MyAccount {
 
 async signOutButtonExists() {
     await t.expect(this.signOutButton.exists).ok('SignOut button was not found.')
-    await t.click(this.signOutButton)
 }
 }export default new MyAccount
